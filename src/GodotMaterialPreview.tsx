26896cefc5fad5847ca0b70e22342b1dba883dda
Daniil Pshegorlinskii <noxormy@gmail.com>
2026-02-07 22:37:19 +0100

    Update GodotMaterialPreview.tsx

--- src/GodotMaterialPreview.tsx
+++ src/GodotMaterialPreview.tsx
@@ -2,10 +2,13 @@ import { forwardRef } from 'react';
 import { GodotMaterialPreviewView } from './GodotMaterialPreviewView';
 import type { GodotMaterialPreviewHandle, GodotMaterialPreviewProps } from './GodotMaterialPreview.types';
 import { useGodotMaterialPreview } from './useGodotMaterialPreview';
+import { RGS_EMBED_FOLDER } from './GodotMaterialPreview.types';
 
+export { RGS_EMBED_FOLDER } from './GodotMaterialPreview.types';
 export type { ShaderUniform, GodotMaterialPreviewHandle, ShaderLoadedPayload, DisplayMode, GodotMaterialPreviewProps } from './GodotMaterialPreview.types';
 
 const DEFAULTS = {
+  embedUrl: `/${RGS_EMBED_FOLDER}/embed.html`,
   previewWidth: 512,
   showMeshSwitch: true,
   allowMouseInteraction: true,
