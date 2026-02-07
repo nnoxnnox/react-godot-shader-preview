428153b474919efdddee10bd3f60be68054f9c16
Daniil Pshegorlinskii <noxormy@gmail.com>
2026-02-07 20:19:03 +0100

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import './GodotMaterialPreview.css';
+
+export { GodotMaterialPreview } from './GodotMaterialPreview';
+export type {
+  GodotMaterialPreviewProps,
+  GodotMaterialPreviewHandle,
+  ShaderUniform,
+  ShaderLoadedPayload,
+  DisplayMode,
+} from './GodotMaterialPreview.types';
+export { validateShader, type ValidationError, type ValidationResult } from './lib';
